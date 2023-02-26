6e28e9ca15a1b6b1422b382ff57ea7290305c735
lazydevcoder <lazydevcoder@gmail.com>
2023-02-26 20:36:03 +0530

    Completed

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+   <div className='headerdiv'>
+    <h4>Get Ready To complete your Daily Goals</h4>
+   </div>
+  )
+}
+
+export default Header
